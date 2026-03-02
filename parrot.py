5bd0d9c9588c811f294682e26204855cea161ecb
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-01 22:00:54 -0500

    implemented break statements

--- parrot.py
+++ parrot.py
@@ -1,14 +1,12 @@
 prompt = "Telleth me something, and I shalt repeat it backe to thee."
 prompt += "\nEntere 'quit' if thou wisheth to stoppe." 
 
-message = ''
-active = True
-while active:
+while True:
     message = input(prompt)
     if message == "quit":
-        active = False
+        break
     if message == "parrot":
         print("Doth thou considereth thine self a wise guy?")
-        active = False
+        break
     else:
         print(message)
