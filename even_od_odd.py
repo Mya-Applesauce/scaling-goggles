6014e0a1d1e91aeb8340210c0025a8f500191d41
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-01 22:11:44 -0500

    adding these files

---
+++ even_od_odd.py
@@ -0,0 +1,6 @@
+number = int(input("Entereth a number, and I shalt telleth thee if it tis even or odd."))
+
+if number % 2 == 0:
+    print(f"\nThe number {number} tis even!")
+else:
+    print(f"\nThe number {number} tis odd!")
