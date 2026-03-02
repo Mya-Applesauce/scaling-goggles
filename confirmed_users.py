0c346f33be0e132664d66d1f757ddea41785363e
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-01 23:24:27 -0500

    back to while loop

--- confirmed_users.py
+++ confirmed_users.py
@@ -1,10 +1,11 @@
 unconfirmed_users = ["sonic", "amy", "knuckles"]
 confirmed_users = []
 
-for unconfirmed_user in unconfirmed_users:
-    print(f"Verifying user: {unconfirmed_user.title()}")
-    confirmed_users.append(unconfirmed_user)
-    unconfirmed_users.remove(unconfirmed_user)
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+
+    print(f"Verifying user: {current_user.title()}")
+    confirmed_users.append(current_user)
 
 print("\nThe following users hath been confirmed")
 for confirmed_user in confirmed_users:
