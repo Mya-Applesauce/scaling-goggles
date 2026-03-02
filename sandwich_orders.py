0b9bcb0c49a65d1f59b7b8d59ab6c0165c564af1
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-02 00:01:55 -0500

    no pastrami

--- sandwich_orders.py
+++ sandwich_orders.py
@@ -1,9 +1,14 @@
-sandwich_orders = ["grilled cheese", "pb&j", "hoagie", "italian", "squid eyeball", "hotdog", "ham on rye", "meatball sub"]
+sandwich_orders = ["grilled cheese", "pastrami", "pastrami", "pastrami", "pb&j", "hoagie", "italian", "squid eyeball", "hotdog", "ham on rye", "meatball sub"]
 finished_sandwiches = []
 
+print("Alas! This deli hast runneth oute of pastrami.")
+while "pastrami" in sandwich_orders:
+    sandwich_orders.remove("pastrami")
+
 for sandwich in sandwich_orders:
-    print(f"Thy {sandwich} sandwich 'tis served.")
+    print(f"We art workinge on thy {sandwich} sandwich now.")
     finished_sandwiches.append(sandwich)
 
+print("These are the sandwiches beinge served to thee: ", end="")
 for sandwiches in finished_sandwiches:
     print(f"{sandwiches}, ", end="")
