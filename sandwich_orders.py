bb5decf4abcde4a0ac2210c62573447986d4285d
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-01 23:43:58 -0500

    initial commit

---
+++ sandwich_orders.py
@@ -0,0 +1,8 @@
+sandwich_orders = ["grilled cheese", "pb&j", "hoagie", "italian", "squid eyeball"]
+finished_sandwiches = []
+
+for sandwich in sandwich_orders:
+    print(f"Thy {sandwich} sandwich 'tis served.")
+    finished_sandwiches.append(sandwich)
+
+print(finished_sandwiches)
